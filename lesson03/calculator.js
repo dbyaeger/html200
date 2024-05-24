4f2ab0f6b7d0e87d9b1881ca9e340623d53e38fc
Daniel Yaeger <danielyaeger@Daniels-MacBook-Air.local>
2024-05-24 12:57:38 -0700

    Assignment 3.1- Calculator.js

---
+++ lesson03/calculator.js
@@ -0,0 +1,11 @@
+const double = 2;
+const triple = 3;
+const pi = 3.14;
+const e = 2.718
+let x = double*pi - triple
+console.log(x)
+x = triple % 2
+console.log(x)
+x = pi**triple + pi**double
+console.log(x)
+console.log((pi*e + pi + e)/triple)

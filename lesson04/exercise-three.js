4863e6e9904e3f81e1a325f480f76028b0c0766c
Daniel Yaeger <danielyaeger@Daniels-MacBook-Air.local>
2024-06-02 21:41:56 -0700

    added exercise-three.js

---
+++ lesson04/exercise-three.js
@@ -0,0 +1,5 @@
+const bluth_array = ['George', 'Oscar', 'Lucille', 'Gob', 'Michael', 'Lindsay', 'Tobias', 'Buster', 'George Michael', 'Maeby'];
+for (i = 0; i < bluth_array.length; i++) {
+    const j = i + 1
+    console.log(j + "," + bluth_array[i])
+};

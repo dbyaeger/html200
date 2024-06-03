9444abf9943346b72e2db1f79c773ea989f9939a
Daniel Yaeger <danielyaeger@Daniels-MacBook-Air.local>
2024-06-02 20:55:56 -0700

    Added lesson4 challenge 1 assigment

---
+++ lesson04/fivestrings.js
@@ -0,0 +1,8 @@
+let bluth_array = ['George', 'Oscar', 'Lucille', 'Gob', 'Michael', 'Lindsay', 'Tobias', 'Buster', 'George Michael', 'Maeby'];
+console.log(bluth_array);
+bluth_array.push('Steve Holt')
+console.log(bluth_array);
+bluth_array.splice(2,1);
+console.log(bluth_array);
+const bluth_str = bluth_array.join(',')
+console.log(bluth_str);
